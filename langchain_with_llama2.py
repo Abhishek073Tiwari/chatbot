e296ee2e6b170187a0a7fc57d00f5b44bbdcf21b
Abhishek073Tiwari <104168169+Abhishek073Tiwari@users.noreply.github.com>
2023-10-27 14:58:32 +0530

    Update langchain_with_llama2.py

--- langchain_with_llama2.py
+++ langchain_with_llama2.py
@@ -186,8 +186,6 @@ from langchain.llms import OpenAI
 
 st.title("🔗 Chatbot")
 
-with st.sidebar:
-
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     st.info(llm(input_text))
